5ecc8fd0b9ef3294719b5c86d43a05372b31e376
Iurii <yurilap4@gmail.com>
2023-12-05 21:55:59 +0300

    Project initialization

---
+++ src/main/java/aroundme/app/AroundmeApplication.java
@@ -0,0 +1,13 @@
+package aroundme.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AroundmeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AroundmeApplication.class, args);
+    }
+
+}
